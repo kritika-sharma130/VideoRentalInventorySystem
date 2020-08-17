d87742c4bfda900433c563cee5c11018370fafd6
kritika-sharma130 <kritika.sharma_cs19@gla.ac.in>
2020-08-17 21:25:57 +0530

    Add a method to get the name of the video.

--- src/definitions/Video.java
+++ src/definitions/Video.java
@@ -12,4 +12,18 @@ public class Video {
     private String videoName;
     private int rating;
     private boolean checkOut;
+
+    //methods
+
+    /**
+     * This method returns the name of the video.
+     *
+     * @return The name of the video.
+     **/
+    public String getName() {
+        return videoName;
+
+    }
+
+
 }
