47a165ffeaa7b2411f38f4b5b4657140bbbcd831
kritika-sharma130 <kritika.sharma_cs19@gla.ac.in>
2020-08-23 17:17:18 +0530

    create the components of the VideoLauncher.java according to the program and this the final commit.

---
+++ src/execution/VideoLauncher.java
@@ -0,0 +1,75 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kritika Sharma
+ *   Date: 17-08-2020
+ *   Time: 20:53
+ *   File: Main.java
+ */
+
+package execution;
+
+import definitions.VideoStore;
+
+import java.util.Scanner;
+
+public class VideoLauncher {
+    private static final int ADD_VIDEO = 1;
+    private static final int CHECK_OUT_VIDEO = 2;
+    private static final int RETURN_VIDEO = 3;
+    private static final int RECEIVE_RATING = 4;
+    private static final int LIST_INVENTORY = 5;
+    private static final int EXIT = 6;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int customerInput;
+        VideoStore myVideoStore = new VideoStore();
+        String videoName;
+        int videoRating;
+        do {
+            System.out.println("MAIN MENU");
+            System.out.println("=========");
+            System.out.println("1. Add video: ");
+            System.out.println("2. Check out video: ");
+            System.out.println("3. Return video: ");
+            System.out.println("4. Receive rating: ");
+            System.out.println("5. List Inventory: ");
+            System.out.println("6. Exit: ");
+            System.out.println("Enter your choice (1..6): ");
+            customerInput = scanner.nextInt();
+            switch (customerInput) {
+                case ADD_VIDEO:
+                    System.out.println("Enter the name of the video you want to add: ");
+                    scanner.nextLine();
+                    videoName = scanner.nextLine();
+                    myVideoStore.addVideo(videoName);
+                    break;
+                case CHECK_OUT_VIDEO:
+                    System.out.println("Enter the name of the video you want to return: ");
+                    scanner.nextLine();
+                    videoName = scanner.nextLine();
+                    myVideoStore.doCheckout(videoName);
+                    break;
+                case RETURN_VIDEO:
+                    System.out.println("Enter the name of the video you want to return: ");
+                    scanner.nextLine();
+                    videoName = scanner.nextLine();
+                    myVideoStore.doReturn(videoName);
+                    break;
+                case RECEIVE_RATING:
+                    System.out.println("Enter the name of the video");
+                    scanner.nextLine();
+                    videoName = scanner.nextLine();
+                    System.out.println("Enter rating: ");
+                    videoRating = scanner.nextInt();
+                    myVideoStore.receiveRating(videoName, videoRating);
+                    break;
+                case LIST_INVENTORY:
+                    myVideoStore.listInventory();
+                    break;
+                default:
+                    break;
+            }
+        } while (customerInput != EXIT);
+        scanner.close();
+    }
+}
