b38dbc35421e2636cc055a46ebe4d634e7290b64
kritika-sharma130 <kritika.sharma_cs19@gla.ac.in>
2020-08-23 16:33:50 +0530

    create VideoStore.java

---
+++ src/definitions/VideoStore.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kritika Sharma
+ *   Date: 23-08-2020
+ *   Time: 16:33
+ *   File: VideoStore.java
+ */
+
+package definitions;
+
+public class VideoStore {
+}
--- src/execution/Main.java
+++ src/execution/Main.java
@@ -8,7 +8,6 @@
 package execution;
 
 import definitions.Video;
-
 public class Main {
     public static void main(String[] args) {
         Video video = new Video("Bahubali:The Beginning");
