562b99d7f764d75538ff020e3a1387d8a8bcbd89
kritika-sharma130 <kritika.sharma_cs19@gla.ac.in>
2020-08-23 16:46:57 +0530

    add components to the definition of VideoStore.java

--- src/definitions/VideoStore.java
+++ src/definitions/VideoStore.java
@@ -7,6 +7,43 @@
 
 package definitions;
 
+import java.util.Arrays;
+
 public class VideoStore {
     private Video[] store;
+
+    public VideoStore() {
+        this.store = new Video[5];
+        for (int i = 0; i < store.length; i++) {
+            store[i] = new Video();
+        }
+    }
+
+    public Video[] getStore() {
+        return store.clone();// .clone() will return a copy of the array,
+        // not the array reference itself.
+    }
+
+    public void setStore(Video[] store) {
+        this.store = store;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(store);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        VideoStore that = (VideoStore) o;
+        return Arrays.equals(getStore(), that.getStore());
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(getStore());
+    }
 }
+
